057905432a83dc2cfa540f56dc550ebf26e2b0fc
Guest User <Guest@Epicodus-10E.local>
2018-07-12 09:27:58 -0700

    Add updated wait time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,11 +22,17 @@ class App extends React.Component {
     clearInterval(this.waitTimeUpdateTimer);
   }
   updateTicketElapsedWaitTime() {
-    // let newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-    // Object.keys(newMasterTicketList).forEach(ticketId => {
-    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-    // });
-    // this.setState({masterTicketList: newMasterTicketList});
+    const { dispatch } = this.props;
+    Object.keys(this.props.masterTicketList).map(ticketId => {
+      const ticket = this.props.masterTicketList[ticketId];
+      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const action = {
+        type: 'UPDATE_TIME',
+        id: ticketId,
+        formattedWaitTime: newFormattedWaitTime
+      };
+      dispatch(action);
+    });
   }
   render() {
     return (
@@ -36,7 +42,7 @@ class App extends React.Component {
           <Route exact path='/' render={()=><TicketList
               ticketList={this.props.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl />} />
-          <Route path='/admin' render={(props)=><Admin  
+          <Route path='/admin' render={(props)=><Admin
               currentRouterPath={props.location.pathname} />} />
           <Route component={Error404} />
         </Switch>
